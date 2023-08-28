391d706ac102b1040cbf009c1248c60e2de52506
Cloud <112786274+Potatooff@users.noreply.github.com>
2023-08-28 12:42:41 -0400

    Create constant.py

---
+++ src/utils/constant.py
@@ -0,0 +1,7 @@
+"""lost words"""
+idk = [
+    "Sorry, i did not understand", "Sorry i didn't get it",
+    "Please could you rephrase it", "Please be more clear",
+    "Please i am still in training", "Sorry i have no idea of what you just said",
+    "My bad, i am not that advanced"
+]
